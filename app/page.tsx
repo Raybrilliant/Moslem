fea075cc11136fca936951eef6e893c9d6fcf071
Raybrilliant <r.f.brilliansyach@gmail.com>
2024-03-20 14:33:57 +0700

    build:Cloudflare

--- app/page.tsx
+++ app/page.tsx
@@ -86,15 +86,13 @@ function convertPrayerTimes(prayer: Prayer) {
   };
 }
 export const runtime = "edge";
-export default async function Home(request:any) {
-  const ip = headers().get("X-Forwarded-For");
+export default async function Home() {
+  const ip = headers().get("cf-connecting-ip");
   const geo = await fetch("https://api.sefinek.net/api/v2/geoip/" + ip).then(
     (res) => res.json()
   );
-  // const prayers: Prayer = await prayerTimeV3(ip == '::1' ? 'Malang' : geo.data.city);
-  const prayers: Prayer = await prayerTimeV3(request.cf.city);
+  const prayers: Prayer = await prayerTimeV3(ip == '::1' ? 'Malang' : geo.data.city);
   const prayer = convertPrayerTimes(prayers);
-  console.log(ip);
 
   // const prayers:Prayer = await prayerTime(req.geo?.city ?? 'Malang',req.geo?.country ?? 'id')
 
